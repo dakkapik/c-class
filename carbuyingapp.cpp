eef11b392cdef278830cfca5829d15344b78930b
Felipe Rodas <ae86rodas@outlook.com>
2022-02-19 18:38:52 -0500

    added rounding function

--- carbuyingapp.cpp
+++ carbuyingapp.cpp
@@ -1,4 +1,12 @@
-#include "pch.h"
+/*
+File name:  carbuyingapp.cpp 
+Author:     Felipe Rodas
+Date:       02/19/2022
+Purpose:    Given price of car, rebate, credit union rate, deal rate and term,
+			use periodic payment formula and return monthly payments for credit 
+			union and dealer.
+*/
+
 #include <iostream>
 #include <math.h>
 
@@ -8,12 +16,12 @@ double getPayment(int, double, int);
 
 int main()
 {
-	double	creditUnionRate = 0.08/12,
-			dealerRate = 0.03/12;
+	double	creditUnionRate,
+			dealerRate;
 
-	int		term = 4,
-			price = 16000,
-			rebate = 3000,
+	int		term,
+			price,
+			rebate,
 			priceAfterRebate;
 
 	cout << "Car Price: ";
@@ -22,24 +30,24 @@ int main()
 	cin >> rebate;
 	cout << "Credit union rate: ";
 	cin >> creditUnionRate;
-	cout << "Dealer rate:";
+	cout << "Dealer rate: ";
 	cin >> dealerRate;
-	cout << "Term in years:";
+	cout << "Term in years: ";
 	cin >> term;
-	
-	creditUnionRate = creditUnionRate / 12;
-	dealerRate = dealerRate / 12;
 
 	priceAfterRebate = price - rebate;
 
 	double	dealerPrice = getPayment(price, dealerRate, term),
 			creditUnionPrice = getPayment(priceAfterRebate, creditUnionRate, term);
 	
-	cout << "Credit union payment: $" << creditUnionPrice
-		<< "\nDealer payment: $" << dealerPrice;
+	dealerPrice = round( dealerPrice * 100.0 ) / 100.0;
+	creditUnionPrice = round( creditUnionPrice * 100.0 ) / 100.0;
+
+	cout << "Credit union monthly payment: $" << creditUnionPrice
+		<< "\nDealer monthly payment: $" << dealerPrice;
 
 }
 
 double getPayment(int principal, double rate, int term) {
-	return (principal * rate / (1 - pow(rate + 1, - (term * 12))));
+	return (principal * (rate/12) / (1 - pow(rate/12 + 1, - (term * 12))));
 }
