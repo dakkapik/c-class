3c4cf91d269f36a39a4cda1c8f6136fab7668cb4
Felipe Rodas <ae86rodas@outlook.com>
2022-03-16 13:52:38 -0400

    Create locateTheLargestElement.cpp

---
+++ locateTheLargestElement.cpp
@@ -0,0 +1,32 @@
+/*
+File name:  locateTheLargestElement.cpp; 
+Author:     Felipe Rodas
+Date:       02/16/2022
+Purpose:   	
+*/
+
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>     
+#include <time.h>  
+
+using namespace std;
+
+const int WIDTH = 4;
+const int HEIGHT = 3;
+
+void locateLargest(const double a[][4], int location[]);
+
+int main()
+{
+
+    double matrix[HEIGHT][WIDTH] = {{23.5, 35, 2, 10},{4.5, 3, 45, 3.5},{35, 44, 5.5, 9.6}};
+    
+
+
+}
+
+void locateLargest(const double a[][4], int location[]){
+    int largest = 0;
+
+}
