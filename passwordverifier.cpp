7c838b17e16743a92e7bd0a392e61e583c528d29
Felipe Rodas <68298003+dakkapik@users.noreply.github.com>
2022-04-06 12:41:59 -0400

    Create passwordverifier.cpp

---
+++ passwordverifier.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+// #include <string>
+#include <cctype>
+
+using namespace std;
+
+
+int main () {
+
+    cout << "Password Requirements0" << 
+    endl << " -The password should be at least 6 characters long" <<
+    endl << " -The password should contain at least one uppercase and at least one lowercase letter." <<
+    endl << " -The password should have at least one digit." << 
+
+
+    endl << "Enter a password: ";
+    return 0;
+}
