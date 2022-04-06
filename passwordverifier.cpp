7d1d8117e464c1ea98c5e658fc82b952efd75530
Felipe Rodas <68298003+dakkapik@users.noreply.github.com>
2022-04-06 16:14:28 -0400

    Update passwordverifier.cpp

--- passwordverifier.cpp
+++ passwordverifier.cpp
@@ -4,15 +4,26 @@
 
 using namespace std;
 
+bool checkPassword(char[]);
+bool checkLenght(char[]);
+bool checkUpperCase(char[]);
+bool checkLowerCase(char[]);
+bool checkDigit(char[]);
 
 int main () {
-
-    cout << "Password Requirements0" << 
+    char password[4];
+    cout << "Password Requirements" << 
     endl << " -The password should be at least 6 characters long" <<
     endl << " -The password should contain at least one uppercase and at least one lowercase letter." <<
     endl << " -The password should have at least one digit." << 
-
-
+    
     endl << "Enter a password: ";
+
+    checkPassword(password);
+
     return 0;
+}
+
+bool checkPassword(char password){
+    if()
 }
